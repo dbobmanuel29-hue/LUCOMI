ec6be3a60f999494ed70c3e325dbac004222a78b
BOBMANUEL TAMUNODIEPRIYE SOGBEYE <dbobmanuel29@gmail.com>
2026-09-24 18:49:13 +0100

    Add smooth page transition animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import { BrowserRouter, Outlet, Route, Routes, useLocation } from "react-router-dom";
 import { FloatingWhatsApp, MobileCTABar, SiteFooter, SiteHeader } from "./components/Chrome";
 import { QuoteProvider } from "./components/QuoteFlow";
@@ -26,11 +27,23 @@ function ScrollToTop() {
 }
 
 function PublicLayout() {
+  const location = useLocation();
+
   return (
     <>
       <SiteHeader />
       <main className="pb-[68px] lg:pb-0">
-        <Outlet />
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -8 }}
+            transition={{ duration: 0.38, ease: [0.22, 0.61, 0.36, 1] }}
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </main>
       <SiteFooter />
       <FloatingWhatsApp />
