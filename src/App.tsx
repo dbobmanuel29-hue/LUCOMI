df146e018b2d94592ecbb6ba597cc3f538de1c4a
BOBMANUEL TAMUNODIEPRIYE SOGBEYE <dbobmanuel29@gmail.com>
2026-09-24 20:28:34 +0100

    Add smoother page transition motion

--- src/App.tsx
+++ src/App.tsx
@@ -38,10 +38,10 @@ function PublicLayout() {
         <AnimatePresence mode="wait" initial={false}>
           <motion.div
             key={location.pathname}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.18, ease: "easeOut" }}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -8 }}
+            transition={{ duration: 0.32, ease: [0.22, 0.61, 0.36, 1] }}
           >
             <Outlet />
           </motion.div>
